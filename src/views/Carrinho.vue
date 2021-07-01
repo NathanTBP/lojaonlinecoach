7cb77dfd8325ce03179700fb30c18032603a1be6
mvellosa <mvellosaa@gmail.com>
2021-07-01 19:12:59 -0300

    db; carrinho

--- src/views/Carrinho.vue
+++ src/views/Carrinho.vue
@@ -3,13 +3,13 @@
     <h1>Carrinho</h1>
     <ul class="w3-ul w3-hoverable">
       <li class="w3-bar" v-for="(product, index) in shopCart" :key="index">
-        <img src="../img/coinC.png" class="w3-bar-item w3-circle" style="width:70px">
+        <!--<img :src="product.imagesource" @click="printaConsole(product)" class="w3-bar-item w3-circle" style="width:3em" /> !-->
         <span class="w3-large w3-bar-item">{{product.name}}</span>
         <input class="w3-medium w3-bar-item w3-border w3-round-large"
         v-model="product.productInfo.quantity"
         type="number"
         min="0"
-        >
+        />
         <span class="w3-large w3-bar-item">R$ {{product.productInfo.price}},00</span>
         <span class="w3-large w3-bar-item">
           R$ {{product.productInfo.price * product.productInfo.quantity}},00
@@ -19,7 +19,7 @@
       </li>
     </ul>
 
-    <button id="purchase-button" type="submit">Finalizar Compra</button>
+    <button id="purchase-button" class="w3-button w3-blue w3-round" type="submit">Finalizar Compra</button>
   </div>
 </template>
 
@@ -27,26 +27,38 @@
 export default {
   data: function() {
     return {
-      carrinho: {},
+      
+      carrinho: [{
+        productId: 1,
+        quantity: 2
+        },
+        {
+        productId: 3,
+        quantity: 1
+        },
+      ],
       shopCart:[{
         name: "Aula iniciante",
-        img: "../img/coinV.png",
-        productInfo: {price: "15", quantity: 1}
+        productInfo: {price: 15, quantity: 1}
       },
       {
         name: "Aula ao Vivo",
-        img: "../img/coinC.png",
-        productInfo: {price: "25", quantity: 1}
+        productInfo: {price: 25, quantity: 1}
       },
       {
         name: "Aula avançada",
-        img: "../img/coinVBronze.png",
-        productInfo: {price: "20", quantity: 1}
+        productInfo: {price: 20, quantity: 1}
       }],
-      total: "0"
+      total: 0,
     }
   },
   methods:{
+    getImgSrc(src){
+      return src
+    },
+    printaConsole: function(product){
+      console.log(product.imagesource)
+    },
     removeFromList: function(index){
       this.shopCart.splice(index, 1)
     }
