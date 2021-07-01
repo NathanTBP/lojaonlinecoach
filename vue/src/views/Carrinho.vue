11ad65b00ff40dbbb42f560d3145d45c0e032fb2
mvellosa <mvellosaa@gmail.com>
2021-07-01 16:57:12 -0300

    parte do carrinho com frontend

--- vue/src/views/Carrinho.vue
+++ vue/src/views/Carrinho.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="page">
+  <div class="page" id="container-carrinho">
     <h1>Carrinho</h1>
-    <!--<ul>
-      <li v-for="(product, index) in shopCart" :key="index">
-        {{product.name}}
-        <ul id="produtos-carrinhos" style="display: inline-block;">
-          <li :v-for="info in product.productInfo" >
-            <input type="number" v-model="info.quantity" min="1" max="20" > x R${{info.price}} =  R$ {{info.price * info.quantity}}
-          </li>
-        </ul>
-        <button id="remove-item-button" @click="removeFromList(index)" style="border-radius: 999px;" >&#10006;</button>
+    <ul class="w3-ul w3-hoverable">
+      <li class="w3-bar" v-for="(product, index) in shopCart" :key="index">
+        <img src="../img/coinC.png" class="w3-bar-item w3-circle" style="width:70px">
+        <span class="w3-large w3-bar-item">{{product.name}}</span>
+        <input class="w3-medium w3-bar-item w3-border w3-round-large"
+        v-model="product.productInfo.quantity"
+        type="number"
+        min="0"
+        >
+        <span class="w3-large w3-bar-item">R$ {{product.productInfo.price}},00</span>
+        <span class="w3-large w3-bar-item">
+          R$ {{product.productInfo.price * product.productInfo.quantity}},00
+        </span>
+        <span @click="removeFromList(product.index)"
+        class="w3-bar-item w3-button w3-xlarge w3-right">&times;</span>
       </li>
-      <div id="separation">----------------------------</div>
-      <li>Total: R${{total}}</li>
     </ul>
-    <br><br><br>
-    <div id="payment-method-box">Cartão de crédito</div>!-->
+
     <button id="purchase-button" type="submit">Finalizar Compra</button>
   </div>
 </template>
@@ -24,30 +27,26 @@
 export default {
   data: function() {
     return {
+      carrinho: {},
       shopCart:[{
-        name: "Aula avançada",
-        productInfo: [{price: "15", quantity: "1"}]
+        name: "Aula iniciante",
+        img: "../img/coinV.png",
+        productInfo: {price: "15", quantity: 1}
       },
       {
         name: "Aula ao Vivo",
-        productInfo: [{price: "25", quantity: "1"}]
+        img: "../img/coinC.png",
+        productInfo: {price: "25", quantity: 1}
       },
       {
         name: "Aula avançada",
-        productInfo: [{price: "20", quantity: "1"}]
+        img: "../img/coinVBronze.png",
+        productInfo: {price: "20", quantity: 1}
       }],
-      total: "100"
+      total: "0"
     }
   },
   methods:{
-    //TODO funcao não consegue mudar o valor do elemento (problema com vuejs)
-    filtrarQuantidade: function(event){
-      let character = event.data
-      //let currentValue = event.target.value
-      if (isNaN(character)){
-          event.target.value = "1"
-      }
-    },
     removeFromList: function(index){
       this.shopCart.splice(index, 1)
     }
