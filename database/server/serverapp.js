2909e8a86af796d169bec06b01696098b6db9373
victorkendi <kendi_victor@usp.br>
2021-07-27 14:55:36 -0300

    db

--- database/server/serverapp.js
+++ database/server/serverapp.js
@@ -7,7 +7,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
-});
+}); //problema cross
 
 // Rotas
 const index= require("./router/home");
